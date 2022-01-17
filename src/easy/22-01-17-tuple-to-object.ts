3a5717dc383a6935dd9641297fb6d18d2781c500
jtr354 <jtr354@foxmail.com>
2022-01-17 22:01:14 +0800

    fix: [tuple to object] error case

--- src/easy/22-01-17-tuple-to-object.ts
+++ src/easy/22-01-17-tuple-to-object.ts
@@ -19,7 +19,7 @@ type cases = [
 // @ts-expect-error
 type error = TupleToObject<[[1, 2], {}]>;
 
-type TupleToObject<T extends readonly any[]> = {
+type TupleToObject<T extends readonly (number | string | symbol)[]> = {
   [P in T[number]]: P;
 };
 
@@ -30,3 +30,12 @@ function tupleToObject(tuple) {
   });
   return obj;
 }
+
+/**
+ * 总结：
+ * 1. typeof 将js的数据类型转换为ts的类型约束
+ * 2. const tuple = ["tesla", "model 3", "model X", "model Y"] as const 中的 `as const` 可以约束每个元素为常量，类似枚举;
+ */
+
+const a = { b: "123" };
+type r = typeof a;
